f619188bdb10483904c1301e1088faf6f21d2cf9
sinofseven <em.s.00001@gmail.com>
2020-11-13 14:20:54 +0900

    一日あたりの労働実績時間の項目を追加

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 kot-remaining-working-hours-per-a-day
 ===
 
+KoTのタイムカード画面に以下の２つを追加します。
+
+- 一日あたりの残り勤務時間
+
 ![sample](images/sample.png)
+
+- Google Chrome: [link](https://chrome.google.com/webstore/detail/kot-remaining-working-hou/peajncoihikhnjcdbaheaffahnhkbded)
+- Firefox: [link](https://addons.mozilla.org/ja/firefox/addon/kot-remai-work-hours-per-a-day/)
--- index.js
+++ index.js
@@ -23,9 +23,34 @@ function main() {
     const paidHoliday = getHolidayCountElementNumber("有休", ".specific-daysCount_1 > li:nth-child(4) > div:nth-child(2)");
     const compensatoryHoliday = getHolidayCountElementNumber("代休", ".specific-daysCount_1 > li:nth-child(5) > div:nth-child(2)");
     const specialLeave = getHolidayCountElementNumber("特別休暇", ".specific-daysCount_1 > li:nth-child(6) > div:nth-child(2)");
+    const workingSum = getElementNumber("労働合計", "body > div > div.htBlock-mainContents > div > div.htBlock-normalTable.specific-table > table > tbody > tr > td.custom13");
 
     console.log("pre calc")
 
+    const oneDateAverageBalanceWorkingHours = calcOneDateAverageBalanceWorkingHours(laborStandardsTime, workday, absence, paidHoliday, compensatoryHoliday, specialLeave, overAndUnderTime)
+    const workedTimePerDay = calcWorkedTimePerDay(workingSum, workday, paidHoliday);
+
+    console.log("pre insert")
+
+    // 挿入処理
+    insertThElement();
+    insertTdElement(oneDateAverageBalanceWorkingHours, workedTimePerDay);
+
+    console.log("complete");
+}
+
+function calcOneDateAverageBalanceWorkingHours(laborStandardsTime, workday, absence, paidHoliday, compensatoryHoliday, specialLeave, overAndUnderTime) {
+    /**
+     * 一日あたりの残労働時間を算出する
+     * @param laborStandardsTime {number} 時間集計 - 労働基準時間
+     * @param workday {number} 日数集計 - 平日
+     * @param absence {number} 日数集計 - 欠勤
+     * @param paidHoliday {number} 日数集計 - 有休
+     * @param compensatoryHoliday {number} 日数集計 - 代休
+     * @param specialLeave {number} 日数集計 - 特別休暇
+     * @param overAndUnderTime {number} 時間集計 - 過不足時間
+     * @return {number}
+     */
     const remainingDays = Math.ceil((laborStandardsTime / 8) - (workday + absence + paidHoliday + compensatoryHoliday + specialLeave));
     console.log(remainingDays);
     let oneDateAverageBalanceWorkingHours = overAndUnderTime < 0 ? 0 : overAndUnderTime / remainingDays;
@@ -33,13 +58,19 @@ function main() {
 
     if (isNaN(oneDateAverageBalanceWorkingHours) || !isFinite(oneDateAverageBalanceWorkingHours)) oneDateAverageBalanceWorkingHours = 0;
 
-    console.log("pre insert")
+    return oneDateAverageBalanceWorkingHours;
+}
 
-    // 挿入処理
-    insertThElement();
-    insertTdElement(oneDateAverageBalanceWorkingHours);
-
-    console.log("complete");
+function calcWorkedTimePerDay(workingSum, workday, paidHoliday) {
+    /**
+     * 一日あたりの労働時間を算出する
+     * @param workingSum {number} 時間集計 - 労働合計
+     * @param workday {number} 日数集計 - 平日
+     * @param paidHoliday {number} 日数集計 - 有休
+     * @return {number}
+     */
+    const workedDays = workday + paidHoliday;
+    return workedDays === 0 ? 0 : workingSum / workedDays;
 }
 
 function getElementNumber(name, selector) {
@@ -69,14 +100,14 @@ function getHolidayCountElementNumber(name, selector) {
     return num;
 }
 
-function createInsertThElement() {
+function createInsertThElement(label) {
     /**
      * 挿入用のTh Elementを作成する
      * @return {HTMLTableHeaderCellElement}
      */
     const th = document.createElement("th");
     const p = document.createElement("p")
-    p.textContent = "一日あたりの残労働時間"
+    p.textContent = label;
     th.appendChild(p);
     return th;
 }
@@ -96,18 +127,23 @@ function createInsertTdElement(hours) {
 }
 
 function insertThElement() {
-    const th = createInsertThElement();
+    const th = createInsertThElement("一日あたりの残労働時間");
+    const th_0 = createInsertThElement("一日あたりの労働実績時間");
     const tr = document.querySelector(".specific-table_800 > thead:nth-child(1) > tr:nth-child(1)");
+    tr.appendChild(th_0);
     tr.appendChild(th);
 }
 
-function insertTdElement(hours) {
+function insertTdElement(hours, workedTimePerDay) {
     /**
      * Td Elementを挿入する
      * @param hours {number} 一日あたりの残労働時間
+     * @param workedTimePerDay {number} 一日あたりの労働時間
      */
     const td = createInsertTdElement(hours);
+    const td_0 = createInsertTdElement(workedTimePerDay);
     const tr = document.querySelector(".specific-table_800 > tbody:nth-child(2) > tr:nth-child(1)");
+    tr.appendChild(td_0);
     tr.appendChild(td);
 }
 
